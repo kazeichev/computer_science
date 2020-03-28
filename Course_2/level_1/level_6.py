d83356b97cb44aa4803782b5f31065864aaf08bd
Nikita Kazeichev <kazeichev@yandex.ru>
2020-03-28 19:28:48 +0300

    Поправил решение задачи

--- Course_2/level_1/level_6.py
+++ Course_2/level_1/level_6.py
@@ -1,12 +1,29 @@
 def WordSearch(length, string, search):
-    words_list = split_string_by_length(string, length)
+    words_list = generate_search_handbook(
+        split_string(string.split(" "), length),
+        length
+    )
+
     return find_substring(words_list, search)
 
 
-def split_string_by_length(string, length):
+def split_string(strings, length):
     words_list = list()
 
-    for word in string.split(" "):
+    for word in strings:
+        if len(word) > length:
+            words_list.append(word[0:length])
+            words_list += split_string([word[length:]], length)
+        else:
+            words_list.append(word)
+
+    return words_list
+
+
+def generate_search_handbook(strings, length):
+    words_list = list()
+
+    for word in strings:
         if len(words_list) == 0:
             words_list.append(word)
         else:

abf7e2b8deeade2537bf619663e2a3d669082aee
Nikita Kazeichev <kazeichev@yandex.ru>
2020-04-12 17:12:28 +0300

    Поправил решение задачи №16

--- Intro_To_Python_And_Algorithms/level_16.py
+++ Intro_To_Python_And_Algorithms/level_16.py
@@ -1,18 +1,22 @@
 def ShopOLAP(n, items):
-    items_dict = generate_items_dict(items)
-    return generate_items_list(sorted(items_dict.items(), key=lambda x: x[0]))
+    items_dict = generate_items_dict(n, items)
+    items_for_sorting = list()
+
+    for i in items_dict:
+        items_for_sorting.append([i, items_dict[i]])
+
+    return generate_items_list(sorted(items_for_sorting, key=lambda x: (-x[1], x[0])))
 
 
-def generate_items_dict(items):
+def generate_items_dict(n, items):
     items_dict = dict()
 
-    for item in items:
-        split_item = item.split(" ")
-
-        if items_dict.get(split_item[0]):
+    for i in range(0, n):
+        split_item = items[i].split(" ")
+        if split_item[0] in items_dict:
             items_dict[split_item[0]] += int(split_item[1])
         else:
-            items_dict[split_item[0]] = int(split_item[1])
+            items_dict.update({str(split_item[0]): int(split_item[1])})
 
     return items_dict
 

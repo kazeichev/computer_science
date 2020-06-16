57274c64a16b8732d6a9b75908f62738445db8b4
Nikita Kazeichev <nkazeychev@unisender.com>
2020-06-16 21:16:57 +0300

    Переделал реализацию функции поиска слова-палиндрома

--- Algorithms_part-1/Deque/is_palindrom.py
+++ Algorithms_part-1/Deque/is_palindrom.py
@@ -3,15 +3,12 @@ from deque import Deque
 
 def is_palindrom(string):
     deque = Deque()
-    is_palindrom = False
 
     for i in list(string):
         deque.addTail(i)
 
     while deque.size() > 1:
-        if deque.removeFront() == deque.removeTail():
-            is_palindrom = True
-        else:
-            is_palindrom = False
+        if deque.removeFront() != deque.removeTail():
+            return False
 
-    return is_palindrom
+    return True

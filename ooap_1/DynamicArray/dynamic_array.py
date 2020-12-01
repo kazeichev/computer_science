f0759464f06e5ac13cf4f503a3904161c5d20b4d
Nikita Kazeichev <nkazeychev@unisender.com>
2020-12-01 21:26:41 +0300

    Добавил описание АТД DynamicArray

---
+++ ooap_1/DynamicArray/dynamic_array.py
@@ -0,0 +1,20 @@
+# АТД DynamicArray
+#
+# abstract class DynamicArray<T>
+#
+#   private int realSize
+#   private int bufferSize
+#   private <T> buffer
+#
+#   public void DynamicArray<T>
+#
+#   public void add()
+#   public void remove()
+#
+#   public <T> get()
+#   public int size()
+#
+#   private void grow()
+#
+#   public int get_add_status()
+#   public int get_remove_status()

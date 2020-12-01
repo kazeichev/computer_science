67406e98f3c06420f6307771b7e643c03fdd7fef
Nikita Kazeichev <nkazeychev@unisender.com>
2020-12-01 21:36:03 +0300

    Правки АТД

--- ooap_1/DynamicArray/dynamic_array.py
+++ ooap_1/DynamicArray/dynamic_array.py
@@ -6,15 +6,24 @@
 #   private int bufferSize
 #   private <T> buffer
 #
-#   public void DynamicArray<T>
+#   public void DynamicArray<T> // конструктор
+#       - постусловие: создан пустой массив
 #
-#   public void add()
-#   public void remove()
-#
-#   public <T> get()
-#   public int size()
-#
+#   // команды
 #   private void grow()
+#   public void add(int i, <T> value) // добавление элемента согласно индексу
+#       - предусловие: i в пределах размера массива
+#       - постусловие: в позицию i добавлено значение value
+#
+#   public void remove(int i) // удаление элемента согласно индексу
+#       - предусловие: массив не пустой, i в пределах размера массива
+#       - постусловие: удален элемент с индексом i
+#
+#   // запросы
+#   public <T> get(int i) // получение элемента по индексу
+#       - предусловие: i в пределах размера массива
+#
+#   public int size() // получение размера массива
 #
 #   public int get_add_status()
 #   public int get_remove_status()

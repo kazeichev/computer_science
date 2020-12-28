31391599c0c21e309ec2f9932e4b831115c26414
Nikita Kazeichev <nkazeychev@unisender.com>
2020-12-28 21:42:16 +0300

    Добавил АТД "NativeDictionary"

---
+++ ooap_1/NativeDictionary/native_dictionary.py
@@ -0,0 +1,22 @@
+# АТД NativeDictionary
+#
+# abstract class NativeDictionary<T>
+#
+#   public void NativeDictionary(int maxSize) - конструктор с параметром максимального размера
+#
+#   // команды
+#   public void insert(string key, <T> value) - добавление нового значения по ключу
+#       - предусловие: в словаре есть место
+#       - постусловие: в словарь добавлен новый элемент по ключу, либо обновлен имеющийся
+#
+#   public void remove(string key) - удаление значения по ключу
+#       - предусловие: словарь не пустой
+#       - постусловие: из словаря удален элемент по ключу
+#
+#
+#   // запросы
+#   public <T> get(string key) - получет элемент по ключу
+#   public int size() - получает размер
+#
+#   public int get_insert_status() - получает статус команды вставки
+#   public int get_remove_status() - получает статус команды удаления

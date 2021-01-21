086c6426ba8b0c0592e43622095581ae4f49eba9
Nikita Kazeichev <nkazeychev@unisender.com>
2021-01-21 17:05:43 +0300

    Добавил реализацию рекурсивных функций

--- Algorithm/RecursiveSum.py
+++ Algorithm/RecursiveSum.py
@@ -1,5 +1,20 @@
 def recursive_sum(arr):
     if len(arr) == 0:
         return 0
+
+    return arr.pop(0) + recursive_sum(arr)
+
+
+def recursive_count(arr):
+    if len(arr) == 0:
+        return 0
+
+    return 1 + recursive_count(arr[1:])
+
+
+def recursive_max(arr):
+    if len(arr) == 2:
+        return arr[0] if arr[0] > arr[1] else arr[1]
     else:
-        return arr.pop(0) + recursive_sum(arr)
+        sum_max = recursive_max(arr[1:])
+        return arr[0] if arr[0] > sum_max else sum_max
